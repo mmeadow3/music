baa4e7f139fbdab209784b0aacc3997ff57ee01d
Mike Meadows <mmeadow33@gmail.com>
2016-06-28 13:54:08 -0500

    new xhr request is rendering on DOM

--- xhr.js
+++ xhr.js
@@ -1,1 +1,16 @@
 
+"use strict";
+var $ajax = $.ajax({
+		url: "music.json"
+	}).done(renderinDOM) 
+
+//console.log($ajax) /////gives jQuery object
+
+function renderinDOM (data){    ///////passes in an argument to parse info from JSON file
+	// console.log(data.firstMessages)
+	// console.log(data.length)
+	for (var i = 0; i < data.length; i++) {
+	$('.main').append("<div id='jsonMusic'>" + data[i].title + "by " + data[i].artist + " on the album " + data[i].album + "</div>");
+}
+};
+////////Data appended to DOM///////////
