d8417aa164dd3cecd2c6f7ad76e5896e19adcea1
Mike Meadows <mmeadow33@gmail.com>
2016-06-29 11:11:16 -0500

    added local jQuery, got buttons to start working

--- javascripts/songs.js
+++ javascripts/songs.js
@@ -1,3 +1,56 @@
+"use strict";
+///////Delete button/////////
+var delButton = $('<button/>')
+  // type: 'button',
+  // class: 'deleteButton',
+  // id: 'delete',    bad to have "static ID on button"
+  .text('Delete')
+  .click(function () { ///////start of function///////
+    $(this).parent().remove();
+    console.log("hey")
+  });
+////////////
+
+
+
+var yarp = "<button>Yarp</button>";
+var $ajax = $.ajax({
+		url: "music.json"
+	}).done(renderinDOM); 
+
+function renderinDOM (data){    ///////passes in an argument to parse info from JSON file
+	console.log(delButton)
+	for (var i = 0; i < data.length; i++) {
+		var newButton = delButton;
+		$('.main').append(`<div class="songsItem">` + data[i].title + " by " + data[i].artist + " on the album " + data[i].album  + "   " + "</div>");
+		$(`.songsItem`).append(newButton);
+	} 
+};
+
+
+////////Data appended to DOM///////////
+/////////////////Add second JSON File when a button is clicked///////////////
+
+var $button = $('<button/>', {
+  type: 'button',
+  class: 'moreButton',
+  id: 'more',
+  text: 'More >',
+  click: function() { ///////start of function///////
+    var $ajax = $.ajax({
+		url: "music2.json"
+	}).done(renderinDOM); 
+
+function renderinDOM (data){    ///////passes in an argument to parse info from JSON file
+	// console.log(data.length)
+	for (var i = 0; i < data.length; i++) {
+	$('.main').append(`<div class="songitem2">` + data[i].title + " by " + data[i].artist + " on the album " + data[i].album  + `</div>`);
+	$(`.songitem2`).append(delButton);
+}
+}
+}
+}).appendTo($('#output')) //////can move with CSS ///////////
+
 
 
 
