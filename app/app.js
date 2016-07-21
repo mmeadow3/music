08668fcfd631ecee309e945f3dc1af869bfe026b
Mike Meadows <mmeadow33@gmail.com>
2016-07-20 20:47:35 -0500

    removed uncessary files

---
+++ app/app.js
@@ -0,0 +1,41 @@
+"use strict";
+
+var app = angular.module("TodoApp", []);///////empty brackets bc there are no dependencies at the moment
+
+
+[
+     {
+       "title": "The Walk",
+       "artist": "Mayer Hawthorne",
+       "album": "How Do You Do"
+     },
+     {
+       "title": "Medicine",
+       "artist": "Grace Potter",
+       "album": "Grace Potter and the Nocturnals"
+     },
+     {
+       "title": "Legs",
+       "artist": "ZZ Top",
+       "album": "Eliminator"
+     }
+   ]
+
+   [
+        {
+          "title": "Songggg",
+          "artist": "Mars Volta",
+          "album": "The Widow"
+        },
+        {
+          "title": "Medicine",
+          "artist": "Phish",
+          "album": "Hoist"
+        },
+        {
+          "title": "Arms",
+          "artist": "ZZ Top",
+          "album": "Elinator"
+        }
+      ]
+    
